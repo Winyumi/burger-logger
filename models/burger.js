15e3b964ee7f7995e95ac1c402280a866fa44224
Winyumi <winguy2003@hotmail.com>
2019-12-21 23:35:51 -0500

    set up model

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function(cb) {
+    orm.selectAll("burgers", function(result) {
+      cb(result);
+    });
+  },
+  create: function(burger_name, cb) {
+    orm.insertOne("burgers", burger_name, function(result) {
+      cb(result);
+    });
+  },
+  update: function(id, devoured, cb) {
+    orm.updateOne("burgers", id, devoured, function(result) {
+      cb(result);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
