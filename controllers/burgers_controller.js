48429e3bf5c7f8a94381f25fe64fc6b8c2702b27
Winyumi <winguy2003@hotmail.com>
2019-12-22 00:32:58 -0500

    check affectedRows instead

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -21,7 +21,7 @@ router.post("/api/burgers", function(req, res) {
 
 router.put("/api/burgers/:id", function(req, res) {
   burger.update(req.params.id, req.body.devoured, function(result) {
-    if (result.changedRows == 0) {
+    if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
@@ -32,7 +32,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
 router.put("/api/burgers", function(req, res) {
   burger.updateAll(req.body.devoured, function(result) {
-    if (result.changedRows == 0) {
+    if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
